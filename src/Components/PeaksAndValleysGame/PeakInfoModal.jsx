d2196623163a9b531c313dbdc9806175d9101623
Ben H <55806947+benharris28@users.noreply.github.com>
2022-10-20 15:25:03 +0000

    Added switch checkbox to allow user to select if they want their own prompt. If they do, a text box will dynamically appear

--- src/Components/PeaksAndValleysGame/PeakInfoModal.jsx
+++ src/Components/PeaksAndValleysGame/PeakInfoModal.jsx
@@ -9,25 +9,33 @@ class PeakInfoModal extends React.Component {
 
   state = {
     prompt: '',
-    time: 30
+    time: 30,
+    promptCheck: true
   }
 
-handlePrompt = (prompt) => {
-        this.setState({
-            prompt: prompt
-        })
-    }
+  handlePrompt = (prompt) => {
+    this.setState({
+      prompt: prompt
+    })
+  }
 
   handleTime = (time) => {
-        this.setState({
-            time: time
-        })
-    }
+    this.setState({
+      time: time
+    })
+  }
+
+  handlePromptCheck = () => {
+    const checked = this.state.promptCheck
+    this.setState({
+      promptCheck: !checked
+    })
+  }
 
   handleSubmit = (e) => {
-   e.preventDefault()
+    e.preventDefault()
 
-    
+
     const prompt = this.state.prompt
     const time = this.state.time
 
@@ -41,87 +49,96 @@ handlePrompt = (prompt) => {
 
     this.props.onHide()
   }
-  
+
   render() {
     const { show, onHide } = this.props;
-  console.log(this.state)
-  
-    
+    console.log(this.state)
+
+
 
     return (
-     <Modal
-      {...this.props}
-      dialogClassName="info-modal"
-      contentClassName="info-modal-content"
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-       backdrop="static"
-      centered>
+      <Modal
+        {...this.props}
+        dialogClassName="info-modal"
+        contentClassName="info-modal-content"
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        backdrop="static"
+        centered>
 
         <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          
-        </Modal.Title>
-      </Modal.Header>
-       <Modal.Body>
-         <h4>How to Play Peaks and Valleys</h4>
-     
-         <div>
-           <form>
-             
-             
-               <div className="margin-bottom">
-                 <Form.Group className="mb-3" controlId="promptInput">
-                <Form.Control type="text" placeholder="test" 
-                  as="textarea" rows={3}
-                  name="prompt"
-                  className="prompt-form-input"
-                  type="text"
-                  placeholder="Type in your topic"
-                  value={this.state.prompt}
-                  onChange={(e) => this.handlePrompt(e.target.value)}
-                  
-                  />
-                
-                 <Form.Text id="passwordHelpBlock" muted>
-        You can choose any topic to talk about. If you can't think of anything, we'll automatically choose a random one for you!
-      </Form.Text>
-                
-                </Form.Group>
-                 </div>
-                <div className="margin-bottom">
-      <Form.Label htmlFor="chooseTime">How long do you want to play for?</Form.Label>
-             <Form.Select aria-label="Default select example"
-               onChange={(e) => this.handleTime(e.target.value)}
-               value={this.state.time}
-               >
-       <option value="5">5 seconds</option>
-      <option value="30">30 seconds</option>
-      <option value="60">1 minute</option>
-      <option value="120">2 minutes</option>
-      <option value="10800">3 hours (I don't have much to do today)</option>
-    </Form.Select>
-                  </div>
-             
-             </form>
-         </div>
+          <Modal.Title id="contained-modal-title-vcenter">
 
-         <div>
-           <Button
-             onClick={this.handleSubmit}
-             >
-             Enter Game
-           </Button>
-         </div>
-       
-       
-         
-      
-       
-         
-         
-       </Modal.Body>
-     </Modal>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>How to Play Peaks and Valleys</h4>
+
+          <div>
+            <form>
+              <Form.Check
+                type="switch"
+                id="custom-switch"
+                label="Choose a topic for me"
+                checked={this.state.promptCheck}
+                onChange={(e) => this.handlePromptCheck(e.target.value)}
+              />
+              {!this.state.promptCheck &&
+
+
+                <div className="margin-bottom">
+                  <Form.Group className="mb-3" controlId="promptInput">
+                    <Form.Control type="text" placeholder="test"
+                      as="textarea" rows={3}
+                      name="prompt"
+                      className="prompt-form-input"
+                      type="text"
+                      placeholder="Type in your topic"
+                      value={this.state.prompt}
+                      onChange={(e) => this.handlePrompt(e.target.value)}
+
+                    />
+
+                    <Form.Text id="passwordHelpBlock" muted>
+                      You can choose any topic to talk about. If you can't think of anything, we'll automatically choose a random one for you!
+                    </Form.Text>
+
+                  </Form.Group>
+                </div>
+              }
+              <div className="margin-bottom">
+                <Form.Label htmlFor="chooseTime">How long do you want to play for?</Form.Label>
+                <Form.Select aria-label="Default select example"
+                  onChange={(e) => this.handleTime(e.target.value)}
+                  value={this.state.time}
+                >
+                  <option value="5">5 seconds</option>
+                  <option value="30">30 seconds</option>
+                  <option value="60">1 minute</option>
+                  <option value="120">2 minutes</option>
+                  <option value="10800">3 hours (I don't have much to do today)</option>
+                </Form.Select>
+              </div>
+
+            </form>
+          </div>
+
+          <div>
+            <Button
+              onClick={this.handleSubmit}
+            >
+              Enter Game
+            </Button>
+          </div>
+
+
+
+
+
+
+
+        </Modal.Body>
+      </Modal>
 
 
 
